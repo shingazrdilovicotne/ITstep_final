202c97e48f6e8c36d88838570d2bd1df9673c8e9
shingazrdilovicotne <utsnobimeksikeli@gmail.com>
2023-05-10 18:38:44 +0400

    cccc

--- main1.js
+++ main1.js
@@ -6,13 +6,12 @@ let title = document.getElementById("title")
 
 
 btn.onclick = function(){
-    name1.style.maxHeight = "0";
+  
     title.innerHTML = "Sign In"
     btn.classList.add("disable")
     btn2.classList.remove("disable")
 }
 btn2.onclick = function(){
-    name1.style.maxHeight = "60px";
     title.innerHTML = "Sign Up"
     btn2.classList.remove("disable")
     btn.classList.add("disable")
