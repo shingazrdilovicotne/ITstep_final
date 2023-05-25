798101253313385269ef39b3502fd965a7d3e398
shingazrdilovicotne <utsnobimeksikeli@gmail.com>
2023-05-25 18:47:50 +0400

    cc

--- main1.js
+++ main1.js
@@ -19,21 +19,21 @@ btn2.onclick = function(){
 }
 
 
-fetch('http://kketelauri-001-site1.gtempurl.com/api/user/adduser', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ 
-        "userName": "keti4",
-    "firstName": "keti4",
-    "lastName": "ketelauri4",
-    "email": "keti4@gmail.com",
-    "privateNumber": "12345678914",
-    "password": "123",
-    "active": true})
-})
-   .then(response => response.json())
-   .then(response => console.log(JSON.stringify(response)))
+// fetch('http://kketelauri-001-site1.gtempurl.com/api/user/adduser', {
+//     method: 'POST',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ 
+//         "userName": "keti4",
+//     "firstName": "keti4",
+//     "lastName": "ketelauri4",
+//     "email": "keti4@gmail.com",
+//     "privateNumber": "12345678914",
+//     "password": "123",
+//     "active": true})
+// })
+//    .then(response => response.json())
+//    .then(response => console.log(JSON.stringify(response)))
 
